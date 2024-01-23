20fbc92544d317289c389e57385dc186869281ab
LucasNekoo <lucasjsp12@gmail.com>
2024-01-23 08:34:06 -0300

    Comentarios

--- TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesLista.cs
@@ -1,30 +1,36 @@
 namespace TestesUnitarios.Desafio.Console.Services
 {
+    // Classe que contém métodos para realizar validações em listas de números inteiros
     public class ValidacoesLista
     {
+        // Remove números negativos da lista fornecida
         public List<int> RemoverNumerosNegativos(List<int> lista)
         {
             var listaSemNegativos = lista.Where(x => x > 0);
             return listaSemNegativos.ToList();
         }
 
+        // Verifica se um número específico está presente na lista
         public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
         {
             var contem = lista.Contains(numero);
             return contem;
         }
 
+        // Multiplica todos os números na lista por um número dado
         public List<int> MultiplicarNumerosLista(List<int> lista, int numero)
         {
             var listaMultiplicada = lista.Select(x => x * numero).ToList();
             return listaMultiplicada;
         }
 
+        // Retorna o maior número na lista
         public int RetornarMaiorNumeroLista(List<int> lista)
         {
             return lista.Max();
         }
 
+        // Retorna o menor número na lista
         public int RetornarMenorNumeroLista(List<int> lista)
         {
             return lista.Min();
--- TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
+++ TestesUnitarios.Desafio.Console/Services/ValidacoesString.cs
@@ -1,19 +1,23 @@
 namespace TestesUnitarios.Desafio.Console.Services
 {
+    // Classe que contém métodos para realizar validações em strings
     public class ValidacoesString
     {
+        // Retorna a quantidade de caracteres na string fornecida
         public int RetornarQuantidadeCaracteres(string texto)
         {
             var numeroCaracteres = texto.Length;
             return numeroCaracteres;
         }
 
+        // Verifica se uma substring específica está presente na string fornecida
         public bool ContemCaractere(string texto, string textoProcurado)
         {
             var contem = texto.Contains(textoProcurado);
             return contem;
         }
 
+        // Verifica se a string fornecida termina com uma substring específica
         public bool TextoTerminaCom(string texto, string textoProcurado)
         {
             var termina = texto.EndsWith(textoProcurado);
